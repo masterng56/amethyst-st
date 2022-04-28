087fa72dc595e0e386598f761f3b5d42e86355b8
masterng <nasterng@yansex.ru>
2022-04-28 09:27:21 +0500

    Базовая структура плагина

---
+++ amethyst-st.php
@@ -0,0 +1,24 @@
+<?php 
+/*
+Plugin Name: Amethyst shortcode
+plugin URL: https://amethyst-st.ru
+Description: plugin for displaying posts via shortcode
+Version: 1.0
+Author: masterng
+Author URL: https://amethyst-st.ru
+Licence: GPLv2 or later
+Text Domain: amt
+*/ 
+
+//доступ к этому файлу только для WordPress если нет константы ABSPATH доступ запрещен
+if(!defined('ABSPATH')){
+    die;
+}
+
+class methystSt{
+    static function activation(){
+        
+    }
+}
+
+?>
